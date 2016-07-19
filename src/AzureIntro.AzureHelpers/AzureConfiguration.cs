c1fd4283de3900435e9e66f4c85d9a44530d8a0d
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2016-07-19 14:51:30 -0500

    Add AzureConfiguration helper

---
+++ src/AzureIntro.AzureHelpers/AzureConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+
+namespace AzureIntro.AzureHelpers
+{
+    public interface IAzureConfiguration
+    {
+        string GetConnectionString(string key);
+
+        string GetAppSetting(string key);
+    }
+
+    /// <summary>
+    /// Class to assist in retrieving app settings if using common azure deployment/config methods.
+    /// App settings will be stored as environment variables and will be overwritten based on azure portal settings.
+    /// </summary>
+    public class AzureConfiguration : IAzureConfiguration
+    {
+        private const string AzureEnvironmentVariableConnectionStringPrefix1 = "CUSTOMCONNSTR_";
+        private const string AzureEnvironmentVariableConnectionStringPrefix2 = "SQLAZURECONNSTR_";
+        private const string AzureEnvironmentVariableAppSettingPrefix = "APPSETTING_";
+
+        public string GetConnectionString(string key)
+        {
+            // Attempt to retrieve environment variable first, then app setting from config file.
+            return
+                new[]
+                {
+                    TryGetEnvironmentVariable(AzureEnvironmentVariableConnectionStringPrefix1, key),
+                    TryGetEnvironmentVariable(AzureEnvironmentVariableConnectionStringPrefix2, key),
+                    TryGetConnectionString(key)
+                }.FirstOrDefault();
+        }
+
+        public string GetAppSetting(string key)
+        {
+            return
+                new[]
+                {
+                    TryGetEnvironmentVariable(AzureEnvironmentVariableAppSettingPrefix, key),
+                    TryGetAppSetting(key)
+                }.FirstOrDefault();
+        }
+
+        private string TryGetEnvironmentVariable(string prefix, string key)
+        {
+            return Environment.GetEnvironmentVariable($"{prefix}{key}");
+        }
+
+        private string TryGetConnectionString(string key)
+        {
+            return ConfigurationManager.ConnectionStrings[key]?.ConnectionString;
+        }
+
+        private string TryGetAppSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
